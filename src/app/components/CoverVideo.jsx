c7719f4e439b6059bfd7fe18c149cebed3ac2632
hassan <hassanbajwa154@gmail.com>
2024-08-03 04:33:24 +0500

    update

--- src/app/components/CoverVideo.jsx
+++ src/app/components/CoverVideo.jsx
@@ -2,20 +2,21 @@ import React from "react";
 
 function CoverVideo() {
   return (
-    <div className="relative">
+    <section className="relative">
       <video
         className="h-[30vh] sm:h-[60vh] w-full object-cover"
         src="/cover-1080p.mp4"
         autoPlay
         muted
         loop
+        aria-label="Cover video showing a dining experience"
       />
       <div className="absolute inset-0 flex items-center justify-center text-white bg-black bg-opacity-50 p-4">
         <h1 className="max-w-[450px] sm:max-w-[500px] text-center text-[33px] leading-[37px] sm:text-[48px] sm:leading-[56px] font-bold px-3 sm:px-0">
-          Revolutionizing The Dining Experience{" "}
+          Revolutionizing The Dining Experience
         </h1>
       </div>
-    </div>
+    </section>
   );
 }
 
