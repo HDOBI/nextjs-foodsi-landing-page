a0920e2bb4192c1d96ef97a5ecbfd318034557f0
hassan <hassanbajwa154@gmail.com>
2024-08-03 08:57:55 +0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      <CoverVideo />
+      {/* <CoverVideo /> */}
       <Service />
       <Testimonial />
       <ContactUS />
